9a83ca5dc3667aa8f5293d35c493e73c1eec03a6
KMen1 <oli70963@gmail.com>
2023-10-01 16:39:14 +0200

    fix: verify token exists

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import { GuildSelector } from "@/components/GuildSelector/GuildSelector";
 import { GuildData } from "@/types";
-import { Container, Title } from "@mantine/core";
+import { Title } from "@mantine/core";
 import { Metadata } from "next";
 import { cookies } from "next/headers";
+import { Container } from "@mantine/core";
 
 export const metadata: Metadata = {
-  title: "Select a server",
-  description: "Welcome to Zeenox",
+  title: "Select a server | Zeenox",
+  description: "Select a server to manage",
 };
 
 export default async function Page() {
@@ -23,6 +24,8 @@ export default async function Page() {
 async function getGuilds() {
   const cookieStore = cookies();
   const serverSessionToken = cookieStore.get("serverSessionToken")?.value;
+  if (!serverSessionToken) throw new Error("No server session token found");
+
   const guildsRes = await fetch(
     `${process.env.BOT_URL}/api/v1/Guild/GetAvailableGuilds`,
     {
